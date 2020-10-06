563d439c4e5e719f6a4b7b5fa2ee9315f8af806f
Brayan David Suárez Restrepo <57419379+brayan2003@users.noreply.github.com>
2020-10-05 22:13:15 -0500

    Actualización frontend

--- al_volante1/src/Pages/Trofeos.js
+++ al_volante1/src/Pages/Trofeos.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-  
+import '../Css/Pantallaprincipal.css'; 
+import '../Css/Trofeos.css'; 
 class Trofeos extends Component {
-    render() {   
+    
+    Pantallaprincipal=()=>{
+        window.location.href="./Pantallaprincipal"; 
+    }
+    Trofeos=()=>{
+        window.location.href="./Trofeos"; 
+    }
+    Raking=()=>{
+        window.location.href="./Raking"; 
+    }
+    Perfil=()=>{
+        window.location.href="./Perfil"; 
+
+    }
+    Donaciones=()=>{
+        window.location.href="./Donaciones"; 
+
+    }
+    render() {
         return (
-            <div>
+            
+            <header className="Pantalla">
+            <div className="title" >
             <p>TROFEOS</p> 
+            <br/>
+            
+            
+            <div className="Trofeo1"> 
+            <p>¡SE EL PRIMERO!
+            <br/>
+            Se obtiene al ganar el primer
+            <br/>
+             puesto
+            </p>
             </div>
-        );
+            <br/>
+            <div className="Barra">
+            <button className="Menu" onClick={()=>this.Pantallaprincipal()}></button>
+            <br/>
+            <button className="Trofeos" onClick={()=>this.Trofeos()}></button>
+            <br/>
+            <button className="Raking" onClick={()=>this.Raking()}></button>
+            <br/>
+            <button className="Perfil" onClick={()=>this.Perfil()}></button>
+            <br/>
+            <button className="Donaciones" onClick={()=>this.Donaciones()}></button>
+            <br/>
+            <button className="Busqueda" ></button>
+            <br/>
+            </div>
+            </div>
+            </header>
+
+    );
     }
 }
-export default Trofeos; 
+export default Trofeos;
